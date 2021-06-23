37dd4a080d7be8c1a38a4862bb710771a4b74d60
anioji94 <ojhaa94@gmail.com>
2021-06-23 16:19:55 +0100

    increase and decrease temeperature methods made and use of private methods

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,18 @@ class Thermostat {
     constructor() {
         this.currentTemperature = 20
     }
+
+    #changeTemperature(value) {
+        this.currentTemperature += value
+    }
+
+    increaseTemperature(value) {
+        this.#changeTemperature(value)
+    }
+
+    decreaseTemperature(value) {
+        this.#changeTemperature(value *= -1)
+    }
 }
 
 module.exports = Thermostat;
--- test/thermostat.test.js
+++ test/thermostat.test.js
@@ -2,10 +2,24 @@ const assert = require('chai').assert;
 const Thermostat = require('../src/thermostat')
 
 describe('Testing Thermostat', function() {
-    describe('Temperature Read-out', () => {
+    describe('#currentTemperature', function() {
         it('should begin with a temperature of 20 degrees', function() {
             let thermostat = new Thermostat;
             assert.strictEqual(thermostat.currentTemperature, 20)
+        })
+    })
+
+    describe('#increaseTemperature/#decreaseTemperature', function() {
+        it('should allow temperature to be increased', function() {
+            let thermostat = new Thermostat;
+            thermostat.increaseTemperature(5)
+            assert.strictEqual(thermostat.currentTemperature, 25)
+        })
+
+        it('should allow temperature to be decreased', function() {
+            let thermostat = new Thermostat;
+            thermostat.decreaseTemperature(5)
+            assert.strictEqual(thermostat.currentTemperature, 15);
         })
     })
     
